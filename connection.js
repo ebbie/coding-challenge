4d3630afa059aa24b704799451c7e33bc31e5c5e
Ebbie <ebenezer.arapally@gmail.com>
2020-09-22 18:57:56 -0400

    Add database environment variable

--- connection.js
+++ connection.js
@@ -1,13 +1,15 @@
 const mysql = require("mysql");
 
-var mysqlConnection = mysql.createConnection({
+var mysqlConnection = process.env.DATABASE_URL || mysql.createConnection('mysql://coding-challenge:Asdfghjkl@35.232.224.199/RxSavingsSolution?debug=true&charset=BIG5_CHINESE_CI&timezone=-0700');
+
+/* var mysqlConnection = mysql.createConnection({
     host: "35.232.224.199",
     user: "coding-challenge",
     password: "Asdfghjkl",
     database: "RxSavingsSolution",
     multipleStatements: true
 })
-
+ */
 mysqlConnection.connect((err) => {
     if (!err) {
         console.log("Connected");
