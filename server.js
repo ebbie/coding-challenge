5937a0e5c26fc6ecabf1e58c693a5e966ead1a14
Ebbie <ebenezer.arapally@gmail.com>
2020-09-22 03:51:18 -0400

    Edit Server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const LocationsRoutes = require("./routes/locations")
 const CalculateRoutes = require("./routes/calculate")
 const mysqlConnection= require("./connection")
-const port = process.env.prot || 5000
+const port = process.env.port || 5000
 
 var app = express();
 app.use(bodyParser.json());
@@ -25,4 +25,4 @@ app.get("/", (req, res)=> {
 app.use("/locations", LocationsRoutes);
 app.use("/calculate", CalculateRoutes);
 
-app.listen(port, ()=> console.log("Express server is running at post no : 3000"));
+app.listen(port, ()=> console.log("Express server is running at post no : 5000"));
