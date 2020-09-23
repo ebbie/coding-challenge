55b6efa1e875e4b1cd6ad7a3b514871460d960b2
Ebbie <ebenezer.arapally@gmail.com>
2020-09-23 00:47:26 -0400

    db config changes

--- connection.js
+++ connection.js
@@ -1,12 +1,14 @@
 const mysql = require("mysql");
 
-var mysqlConnection = mysql.createConnection({
+var mysqlConnection = process.env.DATABASE_URL || mysql.createConnection('mysql://coding-challenge:Asdfghjkl@35.232.224.199/RxSavingsSolution?debug=true&charset=BIG5_CHINESE_CI&timezone=-0700');
+
+/* var mysqlConnection = mysql.createConnection({
     host: "35.232.224.199",
     user: "coding-challenge",
     password: "Asdfghjkl",
     database: "RxSavingsSolution",
     multipleStatements: true
-})
+}) */
 
 mysqlConnection.connect((err) => {
     if (!err) {
