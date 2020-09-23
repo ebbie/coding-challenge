1dca231f7857269ec15262d953a364fc1d3ada52
Ebbie <ebenezer.arapally@gmail.com>
2020-09-23 02:10:40 -0400

    dbconfig createpool added

--- connection.js
+++ connection.js
@@ -1,13 +1,14 @@
-const mysql = require("mysql");
+// const mysql = require("mysql");
+const { createPool } = require('mysql')
 
-var mysqlConnection = process.env.DATABASE_URL || mysql.createConnection('mysql://b24f8644abd1dc:19306b25@us-cdbr-east-02.cleardb.com/heroku_398b6021c3fb5aa?debug=true&charset=BIG5_CHINESE_CI&timezone=-0700');
+//var mysqlConnection = process.env.DATABASE_URL || mysql.createConnection('mysql://b24f8644abd1dc:19306b25@us-cdbr-east-02.cleardb.com/heroku_398b6021c3fb5aa?debug=true&charset=BIG5_CHINESE_CI&timezone=-0700');
 
-mysqlConnection.connect((err) => {
-    if (!err) {
-        console.log("Connected");
-    } else {
-        console.log("Connection Failed" + JSON.stringify(err, undefined, 2));
-    }
+var mysqlConnection = createPool({
+    host: "us-cdbr-east-02.cleardb.com",
+    user: "b24f8644abd1dc",
+    password: "19306b25",
+    database: "heroku_398b6021c3fb5aa",
+    connectionLimit: 10
 })
 
 module.exports = mysqlConnection;
