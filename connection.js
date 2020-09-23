31bf79beeed69e40edbfea9b33f61c13cf4d071b
Ebbie <ebenezer.arapally@gmail.com>
2020-09-23 00:59:56 -0400

    db config changes

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 
-var mysqlConnection = process.env.DATABASE_URL || mysql.createConnection('mysql://coding-challenge:Asdfghjkl@35.232.224.199/RxSavingsSolution?debug=true&charset=BIG5_CHINESE_CI&timezone=-0700');
+var mysqlConnection = process.env.DATABASE_URL || mysql.createConnection('mysql://b24f8644abd1dc:19306b25@us-cdbr-east-02.cleardb.com/heroku_398b6021c3fb5aa?debug=true&charset=BIG5_CHINESE_CI&timezone=-0700');
 
 /* var mysqlConnection = mysql.createConnection({
     host: "35.232.224.199",
